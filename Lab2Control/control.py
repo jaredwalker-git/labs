6b04332c5d60ae86e7abbe1c263c88623efe348e
jaredwalker-git <jwalker1599@gmail.com>
2022-03-26 09:58:40 -0400

    setup package, created control file, edited Dockerfile

---
+++ Lab2Control/control.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import rospy
+
+
+def forward(): #function to move 1m
+
+def turn(): #function to turn 90 degrees
+
+def wait(): #function to wait 5s
+
+
+if __name__ == '__main__':
+
