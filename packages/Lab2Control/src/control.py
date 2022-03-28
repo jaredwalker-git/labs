932fd744831935c3bfe8333f7c80472ce7a56c38
jaredwalker-git <jwalker1599@gmail.com>
2022-03-28 18:34:45 -0400

    finished first draft of code

---
+++ packages/Lab2Control/src/control.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import rospy
+import time
+from duckietown_msgs.msg import WheelCmdStamped
+
+class RosAgent:
+    self.publisher = rospy.Publisher('lane_controller_node/car_cmd', WheelsCmdStamped)
+    self.subscriber = rospy.Subscriber('fsm_node/mode', WheelsCmdStamped, self.callback)
+    
+    def callback(self): #set for half of the square currently
+        forward()
+        wait()
+        turn()
+        forward()
+        wait()
+
+    def forward(self): #function to move 1m
+        self.publisher.Publish('{vel_left: 0.47,vel_right: 0.47}')
+        time.sleep(3) #time initialized at 3s for first attempt
+        self.publisher.Publish('{vel_left: 0.0,vel_right: 0.0}')
+        
+    def turn(self): #function to turn 90 degrees right
+        self.publisher.Publish('{vel_left: 0.23,vel_right: 0.0}') #turn half speed for accuracy
+        time.sleep(1) #1s is first initialization for turn time
+        self.publisher.Publish('{vel_left: 0.0,vel_right: 0.0}')
+
+    def wait(self): #function to wait 5s
+    	time.sleep(5)
+
+
+if __name__ == '__main__':
+    rospy.init_node('onemetersquare')
+    rospy.spin()
+    agent = RosAgent()
+
